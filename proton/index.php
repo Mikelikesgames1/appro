004d6b520786ff7b9117d6b81b27acf88c2685c0
Mikelikesgames1 <91957076+Mikelikesgames1@users.noreply.github.com>
2023-03-09 16:14:27 -0500

    Add files via upload

---
+++ proton/index.php
@@ -0,0 +1,76 @@
+
+<html>
+  <head>
+    <title>Atom Portal :)</title>
+  <link href="/proton/style.css" rel="stylesheet" type="text/css" media="all">
+<script async src="https://arc.io/widget.min.js#WpKp1L5M"></script>
+    <script>
+alert("ALERT THIS PAGE IS UNDER RENNOVATION BUTTONS MAY BE BROKEN");
+      </script>
+    <audio id="myAudio" loop>
+		<source src="../music.mp3" type="audio/mpeg">
+	</audio>
+
+	<center><button class="offlinemc" onclick="playAudio()">Play Background Music</button></center>
+    <p></p>
+	<center><button class="onlinemc" onclick="stopAudio()">Stop Background Music</button></center>
+    <center><a href="../index.php"><button class="goback">Go Back</button></a></center>
+	<script>
+		var audio = document.getElementById("myAudio");
+
+		function playAudio() {
+			audio.play();
+		}
+
+		function stopAudio() {
+			audio.pause();
+			audio.currentTime = 0;
+		}
+	</script>
+    <p></p>
+    <center><a href="/proton/requestgames/requestgames.php"><button class="goback">Request Games</button></a></center>
+    <p></p>
+<a href="/proton/minecraft/EaglerX_Offline.html"><input type="image" name="Minecraft1.8" src="/proton/img/minecraft18.png" alt="Mc 1.8">
+
+<a href="/proton/minecraft/Eagler_Offline.html"><input type="image" name="Minecraft1.5.2" src="/proton/img/mc152.png" alt="Mc1.8">
+
+<a href="/proton/run3/index.php"><input type="image" name="run3" src="/proton/img/run3.png" alt="Run 3">
+
+<a href="/proton/yohoho/index.php"><input type="image" name="yohoho.io" src="/proton/img/yohoho.png" alt="YoHoHo.IO (Working)">
+
+<a href="/proton/alchemy/index.php"><input type="image" name="Little Alchemy" src="/proton/img/little.png" alt="Little Alchemy">
+   
+<a href="/proton/bballstars/index.php"><input type="image" name="BasketBall Stars" src="/proton/img/bbstars.png" alt="Basketball Stars">
+   
+<a href="/proton/bitlife/index.php"><input type="image" name="Bitlife" src="/proton/img/bitlife.png" alt="Bitlife">
+    
+<a href="/proton/flappy-bird/index.php"><input type="image" name="Flappy Bird" src="/proton/img/flappy.png" alt="Flappy Bird">
+    
+<a href="/proton/madalin-stunt-cars-2/index.php"><input type="image" name="Madalin Stunt Cars 2" src="/proton/img/madalin2.png" alt="Madalin Stunt Cars 2">
+    
+<a href="/proton/madalin-stunt-cars-3/index.php"><input type="image" name="Madalin Stunt Cars 3" src="/proton/img/madalin3.png" alt="Madlin Stunt Cars 3">
+    
+<a href="/proton/retro-bowl/index.php"><input type="image" name="Retro Bowl" src="/proton/img/retro.png" alt="Retro Bowl">
+   
+<a href="/proton/slope/index.php"><input type="image" name="Slope" src="/proton/img/slope.png" alt="Slope">
+
+<a href="/proton/slope-2/index.php"><input type="image" name="Slope 2" src="/proton/img/slope2.png" alt="Slope 2">
+
+  <a href="/proton/space-company/index.php"><input type="image" name="Space Company" src="/proton/img/spaceco.png" alt="Space Company">
+    
+  <a href="/proton/subway-surfers/index.php"><input type="image" name="Subway Surfers" src="/proton/img/subway.png" alt="Subway Surfer">
+   
+  <a href="/proton/superhot/index.php"><input type="image" name="Superhot" src="/proton/img/super.png" alt="Superhot">
+    
+  <a href="/proton/townscaper/index.php"><input type="image" name="Townscape" src="/proton/img/towns.png" alt="Townscaper">
+       
+  <a href="https://sarah.sarahsblog.10001mb.com"><input type="image" name="FNF" src="/proton/img/fnf.png" alt="FNF">
+    
+  <a href="/proton/gd/index.php"><input type="image" name="Geometry Dash" src="/proton/img/gd.png" alt="Geometry Dash">
+    
+<p></p>
+    
+    <center><a href="/proton/index2.php"><button class="playminecraft">Next Page</button></a></center>
+    
+  </body>
+</html>
---
+++ proton/underdev.php
@@ -0,0 +1,2 @@
+Sorry :( This Page Is Under Development. 
+<script async src="https://arc.io/widget.min.js#xGQdN1dR"></script>
