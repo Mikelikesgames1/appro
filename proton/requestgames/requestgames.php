429dc6bdb3452125e4abd32c99664826ada39b68
Mikelikesgames1 <91957076+Mikelikesgames1@users.noreply.github.com>
2023-03-09 16:19:49 -0500

    Add files via upload

---
+++ proton/requestgames/requestgames.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Games Suggestions Form</title>
+</head>
+<body>
+	<h1>What Games Should We Add?</h1>
+	<center><form action="submit.php" method="post">
+		<label for="name">Enter your name:</label><br>
+		<input type="text" id="name" name="name"><br>
+    <p></p>
+		<label for="movie-suggestion">Enter your Game suggestion:</label><br>
+		<textarea id="movie-suggestion" name="movie-suggestion" rows="5" cols="50"></textarea><br>
+		<input type="submit" value="Submit">
+	</form></center>
+  <style>body {
+	font-family: Arial, sans-serif;
+}
+
+h1 {
+	text-align: center;
+}
+
+form {
+	width: 50%;
+	margin: 0 auto;
+}
+
+label {
+	display: block;
+	margin-bottom: 10px;
+}
+
+input[type="text"],
+textarea {
+	padding: 10px;
+	border-radius: 5px;
+	border: 1px solid #ccc;
+	width: 100%;
+	box-sizing: border-box;
+}
+
+input[type="submit"] {
+	background-color: #4CAF50;
+	color: white;
+	padding: 10px 20px;
+	border: none;
+	border-radius: 5px;
+	cursor: pointer;
+}
+
+input[type="submit"]:hover {
+	background-color: #45a049;
+}
+
+.error-message {
+	color: red;
+	font-weight: bold;
+}
+</style>
+</body>
+</html>
+
